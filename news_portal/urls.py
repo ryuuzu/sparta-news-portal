138b170305e75680518c34368510fa9564d45cf0
ardentcprog <np01cp4s210045@islingtoncollege.edu.np>
2022-12-07 20:56:54 +0545

    Added Forms submission and their render logic

--- news_portal/urls.py
+++ news_portal/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('forex/', views.forexapi, name="Forex"),
     path('horoscope/', views.horoscopeapi, name="Horoscope"),
     path('weather/', views.weatherapi, name="Weather"),
+    path('addnews/', views.addNews, name="addnews"),
+    path('reportnews/<int:pk>', views.reportNews, name="reportnews"),
+    path('addcomment/<int:pk>', views.addComment, name="addcomment"),
+    path('addevidence/<int:pk>', views.addEvidence, name="addevidence"),
+    path('requestad/', views.requestAd, name="requestad"),
 ]
